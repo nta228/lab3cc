300fb9622c0ae0d674d6a12ab9524226f59a81fe
nta228 <64982003+nta228@users.noreply.github.com>
2021-01-06 06:35:10 +0700

    ss7

---
+++ ex6ss7.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeSnipet6
+{
+    class Animal
+    {
+        public Animal()
+        {
+            Console.WriteLine("Animal constructor without parameters");
+        }
+        public Animal(String name)
+        {
+            Console.WriteLine("Animal constructor with a string parameter");
+        }
+    }
+    class Canine : Animal
+    {
+        //base() takes a string value called “Lion”
+        public Canine() : base("Lion")
+        {
+            Console.WriteLine("Derived Canine");
+        }
+    }
+    class Details
+    {
+        static void Main(String[] args)
+        {
+            Canine objCanine = new Canine();
+        }
+    }
+}
